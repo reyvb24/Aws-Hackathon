d3009c95995765949d58de15b337e7be30a10073
vieriharney <vieriharney@gmail.com>
2021-02-27 00:58:39 +0800

    update home

--- botelle/lib/home.dart
+++ botelle/lib/home.dart
@@ -10,7 +10,34 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Botelle"),
+        title: Text(
+          "Botelle",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: PageView(
+        children: [Text("Hi")],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text("Partners"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_membership),
+            title: Text("Membership"),
+          ),
+        ],
       ),
     );
   }
