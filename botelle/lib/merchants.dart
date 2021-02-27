3aac80db61a12c2c3d2a08ceaa6a617c0eaa438c
renoandrianto <ren0@connect.hku.hk>
2021-02-27 20:53:33 +0800

    Bottom navigation bar

--- botelle/lib/merchants.dart
+++ botelle/lib/merchants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class Merchants extends StatefulWidget {
   @override
